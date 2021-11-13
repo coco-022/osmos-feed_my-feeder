5c3c2b712d6e3a3b4102707c50ebc7b2b01fbea8
こここ <67046760+coco-022@users.noreply.github.com>
2021-11-13 16:32:34 +0900

    Update index.js

--- static/index.js
+++ static/index.js
@@ -1,1 +1,14 @@
-// Wrtie your code here.
+var mySwiper = new Swiper(".swiper-container", {
+  height: 400,
+  slidesPerView: 1.3,
+  spaceBetween: 15,
+  centeredSlides: true,
+  touchAngle: 45,
+  breakpoints: {
+    // when window width is <= 640px
+    900: {
+      slidesPerView: 2,
+      spaceBetween: 15,
+    },
+  },
+});
