e722ee72145c4c095b668d5837d56d60ba3cb93e
こここ <67046760+coco-022@users.noreply.github.com>
2021-11-22 13:31:35 +0900

    Update index.js

--- static/index.js
+++ static/index.js
@@ -1,7 +1,7 @@
 var mySwiper = new Swiper(".swiper-container", {
   height: 400,
-  slidesPerView: 1.1,
-  spaceBetween: 30,
+  slidesPerView: 1.3,
+  spaceBetween: 50,
   centeredSlides: true,
   touchAngle: 45,
   navigation: {
@@ -11,8 +11,8 @@ var mySwiper = new Swiper(".swiper-container", {
   breakpoints: {
     // when window width is <= 640px
     900: {
-      slidesPerView: 1.1,
-      spaceBetween: 30,
+      slidesPerView: 1.3,
+      spaceBetween: 50,
     },
   },
 });
